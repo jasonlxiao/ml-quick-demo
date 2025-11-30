4373258ece66fb93a109aa7e98dc70ab2fd9eaa0
Jason Xiao <124097694+jasonlxiao@users.noreply.github.com>
2025-11-29 21:18:22 -0600

    Change random_state in train_test_split to 0

--- logistic_demo.py
+++ logistic_demo.py
@@ -9,7 +9,7 @@ X = iris.data
 y = iris.target
 
 # Split data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
 # Train model
 model = LogisticRegression(max_iter=200)
